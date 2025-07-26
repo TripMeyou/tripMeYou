0ac6dc7fa1dde2106abef1dfc4b9ed155b88d260
강동연 <kdy2541@naver.com>
2025-07-26 10:10:51 +0900

    test: azurecloudDB test

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id, user_name, user_password, user_email, user_tel, created_date)
+VALUES ('admin', '관리자', 'encryptedPassword123', 'admin@example.com', '010-0000-0000', NOW());
+--초기 테스트용
